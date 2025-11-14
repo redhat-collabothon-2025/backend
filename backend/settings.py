68f0a12467cde8b5c2dd87ccbe4394a54e98cd43
vladyusa <usatenkovladislav33@gmail.com>
2025-11-14 14:58:27 +0100

    Fixes

--- backend/settings.py
+++ backend/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'whitehat_app.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
