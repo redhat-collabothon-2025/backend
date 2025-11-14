ea6cce5665df01bad847b9527f923583d9b5f58b
vladyusa <usatenkovladislav33@gmail.com>
2025-11-14 15:52:55 +0100

    Added campaigns module

--- whitehat_app/urls.py
+++ whitehat_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('auth/', include('whitehat_app.auth.urls'))
+    path('auth/', include('whitehat_app.auth.urls')),
+    path('campaigns/', include('whitehat_app.campaigns.urls')),
 ]
